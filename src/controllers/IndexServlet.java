dc9231a6a8b9f1ac4d3268c4677b0e533fdf1c07
higuc <higuc@192.168.0.32>
2022-05-04 19:47:02 +0900

    フラッシュメッセージを追加

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -40,6 +40,13 @@ import utils.DBUtil;
 	        em.close();
 	        request.setAttribute("tasks", tasks);
 
+	     // フラッシュメッセージがセッションスコープにセットされていたら
+	        // リクエストスコープに保存する（セッションスコープからは削除）
+	        if(request.getSession().getAttribute("flush") != null) {
+	            request.setAttribute("flush", request.getSession().getAttribute("flush"));
+	            request.getSession().removeAttribute("flush");
+	        }
+
 	        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/index.jsp");
 	        rd.forward(request, response);
 	}
